ec091a562222cd7bc241ccb6cb7196d8a41c551a
martinvrbka <martin.vrbkaa@seznam.cz>
2022-03-06 12:46:07 +0100

    auth

--- auth.py
+++ auth.py
@@ -5,7 +5,7 @@ API_KEY = 'af2ca902-9d42-11ec-ab40-23e9218cb75d'
 def api_key_required(decorated_method):
     def wrapper(*args, **kwargs):
         for i in request.headers:
-            if i[0] == 'API_KEY' and i[1] == API_KEY:
+            if i[0] == 'Api-Key' and i[1] == API_KEY:
                 return decorated_method(*args, **kwargs)
         else:
             return {'error':'valid API key missing!'}, 401
